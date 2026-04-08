2a0dfa0c0483d47f502cf2a8696d5de790300f08
kineticnapier <kiner5674@gmail.com>
2026-04-09 02:22:56 +0900

    portテスト

---
+++ InputDevice/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.IO.Ports;
+
+namespace InputDevice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using var port = new SerialPort("COM4", 115200);
+            port.Open();
+            var strings = "hello world";
+            foreach (var s in strings)
+            {
+                Thread.Sleep(1000);
+                port.Write(s.ToString());
+            }
+
+            port.Close();
+        }
+    }
+}
